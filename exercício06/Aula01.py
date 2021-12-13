fe83ccdc84a03ede557344a4dcf82c1c1154f2a6
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2021-12-13 14:39:35 -0300

    Funções (def) em Python - Parte 2

---
+++ exercício06/Aula01.py
@@ -0,0 +1,27 @@
+
+# def func():
+#     print(variable)
+#
+# def func2(arg=None):
+#     arg = arg.replace("v", "c")
+#     return arg
+#
+#
+# func()
+# outra_variavel = func2(arg=variable)
+#
+# print(outra_variavel)
+#
+
+def func():
+    another_value = "João Pedro"
+    return another_value
+
+def func2(args):
+    print(args)
+
+var = func()
+
+func2(var)
+
+
